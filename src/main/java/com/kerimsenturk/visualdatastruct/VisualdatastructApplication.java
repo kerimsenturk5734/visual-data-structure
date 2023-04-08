0d19056f4ab69a3f93fcb0d722fb57adff6c43c1
Kerim Şentürk <kerimsenturk2002@outlook.com>
2023-04-08 17:24:43 +0300

    SwaggerConfig

--- src/main/java/com/kerimsenturk/visualdatastruct/VisualdatastructApplication.java
+++ src/main/java/com/kerimsenturk/visualdatastruct/VisualdatastructApplication.java
@@ -3,8 +3,10 @@ package com.kerimsenturk.visualdatastruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+@EnableSwagger2
 public class VisualdatastructApplication {
 
 	public static void main(String[] args) {
