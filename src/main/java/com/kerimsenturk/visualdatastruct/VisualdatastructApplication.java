0f2a4f071aa22220925dfa6fb61306af46fb39c5
Kerim Şentürk <kerimsenturk2002@outlook.com>
2023-04-19 22:23:28 +0300

    Password encoder and endpoint authorization implemented in configure method of WebSecurityConfig

--- src/main/java/com/kerimsenturk/visualdatastruct/VisualdatastructApplication.java
+++ src/main/java/com/kerimsenturk/visualdatastruct/VisualdatastructApplication.java
@@ -2,10 +2,9 @@ package com.kerimsenturk.visualdatastruct;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+@SpringBootApplication()
 @EnableSwagger2
 public class VisualdatastructApplication {
 
--- src/main/java/com/kerimsenturk/visualdatastruct/auth/WebSecurityConfig.java
+++ src/main/java/com/kerimsenturk/visualdatastruct/auth/WebSecurityConfig.java
@@ -1,11 +1,16 @@
 package com.kerimsenturk.visualdatastruct.auth;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
@@ -14,17 +19,46 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final JwtTokenFilter tokenFilter;
+    private final UserDetailsServiceCustom userDetailsServiceCustom;
 
-    public WebSecurityConfig(JwtTokenFilter tokenFilter) {
+    public WebSecurityConfig(JwtTokenFilter tokenFilter, UserDetailsServiceCustom userDetailsServiceCustom) {
         this.tokenFilter = tokenFilter;
+        this.userDetailsServiceCustom = userDetailsServiceCustom;
     }
 
+    @Bean
+    public DaoAuthenticationProvider authProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsServiceCustom);
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager getAuthManager() throws Exception {
+       return super.authenticationManager();
+    }
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
-                .authorizeRequests().antMatchers("/v1/api/users/login","/v1/api/users/register")
-                .authenticated()
-                .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+                    .authorizeRequests()
+                    .antMatchers("/v1/api/users/login","/v1/api/users/register")
+                    .permitAll()
+
+                .and()
+                    .antMatcher("/v1/api/**")
+                    .authorizeRequests()
+                    .anyRequest()
+                    .authenticated()
+
+                .and()
+                    .sessionManagement()
+                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
         http.addFilterBefore(tokenFilter, UsernamePasswordAuthenticationFilter.class);
     }
